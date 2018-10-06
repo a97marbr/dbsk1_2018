07a72e8b220c63ca9873acd7be43a162e43cd692
Marcus Brohede <a97marbr@gmail.com>
2018-10-06 14:24:10 +0200

    Seed code

---
+++ dbsk1_2018/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using dbsk1_2018.Models;
+
+namespace dbsk1_2018.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
